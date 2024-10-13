fdbc453f8c4fef69add77e23c50e1d04f881fcb2
Kooreis <kooreis.official@gmail.com>
2024-10-13 09:58:45 -0500

    In this update, we create a Queue and enqueue some tasks. The tasks are simply strings representing the task names.

--- solution.cs
+++ solution.cs
@@ -1,15 +1,9 @@
-Here is a simple console application in C# that simulates a task scheduler with cooldown intervals. This application uses a Queue to manage tasks and a Timer to manage cooldown intervals.
-
-```C#
 using System;
 using System.Collections.Generic;
-using System.Timers;
 
 class Program
 {
     static Queue<string> tasks = new Queue<string>();
-    static Timer timer;
-    static int cooldown = 2000;
 
     static void Main(string[] args)
     {
@@ -19,27 +13,6 @@ class Program
         tasks.Enqueue("Task 4");
         tasks.Enqueue("Task 5");
 
-        timer = new Timer(cooldown);
-        timer.Elapsed += ExecuteTask;
-        timer.Start();
-
         Console.ReadLine();
     }
-
-    static void ExecuteTask(object sender, ElapsedEventArgs e)
-    {
-        if (tasks.Count > 0)
-        {
-            string task = tasks.Dequeue();
-            Console.WriteLine($"{DateTime.Now}: {task} executed");
-        }
-        else
-        {
-            timer.Stop();
-            Console.WriteLine("All tasks executed");
-        }
-    }
 }
-```
-
-In this application, we first enqueue some tasks into a Queue. We then start a Timer with a cooldown interval. When the Timer elapses, we execute the next task in the Queue. If there are no more tasks in the Queue, we stop the Timer.
