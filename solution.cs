49680212922a56de50cf2a7928e9189f34227d5e
Kooreis <kooreis.official@gmail.com>
2024-10-14 10:58:45 -0500

    In this update, we add a Timer and a cooldown interval. The Timer is not started yet.

--- solution.cs
+++ solution.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Timers;
 
 class Program
 {
     static Queue<string> tasks = new Queue<string>();
+    static Timer timer;
+    static int cooldown = 2000;
 
     static void Main(string[] args)
     {
@@ -12,6 +15,8 @@ class Program
         tasks.Enqueue("Task 3");
         tasks.Enqueue("Task 4");
         tasks.Enqueue("Task 5");
+
+        timer = new Timer(cooldown);
 
         Console.ReadLine();
     }
